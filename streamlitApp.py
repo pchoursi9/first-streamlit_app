e47a42979765f918b583e2701b01e32546ff6748
pchoursi9 <124370902+pchoursi9@users.noreply.github.com>
2023-02-03 14:23:27 +0000

    Update streamlitApp.py
    menu

--- streamlitApp.py
+++ streamlitApp.py
@@ -4,3 +4,8 @@ import streamlit
 streamlit.title("My first git and streamlit set-up")
 
 streamlit.text("first code")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
