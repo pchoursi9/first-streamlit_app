b52e08ad7b30b15ece744d50a93b3db898a0a7a0
pchoursi9 <124370902+pchoursi9@users.noreply.github.com>
2023-02-03 14:26:38 +0000

    Update streamlitApp.py
    test first python code

--- streamlitApp.py
+++ streamlitApp.py
@@ -1,11 +1,8 @@
 
 import streamlit
 
-streamlit.title("My first git and streamlit set-up")
+import pandas
 
-streamlit.text("first code")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.dataframe(my_fruit_list)
